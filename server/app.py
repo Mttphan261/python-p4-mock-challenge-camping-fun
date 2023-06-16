4ebede7c299b509ed75705937cbcb59a8269c0ed
Matthew Phan <mttphan261@gmail.com>
2023-06-15 22:54:44 -0500

    attempt 1 complete

--- server/app.py
+++ server/app.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 
+from models import db, Activity, Camper, Signup
+from flask_restful import Api, Resource
+from flask_migrate import Migrate
+from flask import Flask, make_response, jsonify, request
 import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-DATABASE = os.environ.get("DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'instance/app.db')}")
+DATABASE = os.environ.get(
+    "DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'instance/app.db')}")
 
-from flask import Flask, make_response, jsonify, request
-from flask_migrate import Migrate
-from flask_restful import Api, Resource
-
-from models import db, Activity, Camper
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE
@@ -19,9 +19,98 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
+api = Api(app)
+
+
 @app.route('/')
 def home():
-    return ''
+    return 'This is the home page'
+
+
+class Campers(Resource):
+
+    def get(self):
+        campers = [c.to_dict(only=('id', 'name', 'age')) for c in Camper.query.all()]
+        return make_response(
+            campers, 200
+        )
+    
+    def post(self):
+        data = request.get_json()
+        try:
+            new_camper = Camper(
+                name=data['name'],
+                age=data['age']
+            )
+
+            db.session.add(new_camper)
+            db.session.commit()
+
+            return make_response(new_camper.to_dict(), 201)
+        except:
+            return { "error": "400: Validation Error"}, 400
+
+
+api.add_resource(Campers, '/campers')
+
+
+class OneCamper(Resource):
+
+    def get(self, id):
+        try:
+            camper = Camper.query.filter_by(id=id).first().to_dict()
+            return make_response( camper, 200)
+        except:
+            return { "error": "404: Camper Not Found" }, 404 
+
+
+api.add_resource(OneCamper, '/campers/<int:id>')
+
+class Activities(Resource):
+
+    def get(self):
+        activities = [a.to_dict() for a in Activity.query.all()]
+        return make_response(activities, 200)
+    
+
+
+api.add_resource(Activities, '/activities')
+
+class OneActivity(Resource):
+
+    def delete(self, id):
+        try:
+            activity = Activity.query.filter_by(id=id).first()
+            #***** Delete signups like the README said? *****
+            # signups = activity.signups
+            # db.session.delete(signups)
+            db.session.delete(activity)
+            db.session.commit()
+            return {}, 204
+        except:
+            return {"error": "404: Activity not found"}, 404
+
+api.add_resource(OneActivity, '/activities/<int:id>')
+
+class Signups(Resource):
+
+    def post(self):
+        data = request.get_json()
+        try:
+            new_signup = Signup(
+                time=data['time'],
+                camper_id=data['camper_id'],
+                activity_id=data['activity_id']
+            )
+            db.session.add(new_signup)
+            db.session.commit()
+            #*****Return activity object???? return make_response(new_signup.activity.to_dict(), 201)*****
+            return make_response(new_signup.to_dict(), 201)
+        except:
+            return {'error': "400: Validation error"}, 400
+
+api.add_resource(Signups, '/signups')
+    
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -19,9 +19,18 @@ db = SQLAlchemy(metadata=metadata)
 class Activity(db.Model, SerializerMixin):
     __tablename__ = 'activities'
 
+    # serialize_rules = ('-signups.activity',)
+    serialize_only = ('id', 'name', 'difficulty')
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     difficulty = db.Column(db.Integer)
+    # created_at = db.Column(db.DateTime, server_default=db.func.now())
+    # updated_at = db.Column(db.DateTime, server_default=db.func.now(), onupdate = db.func.now())
+
+    #****RELATIONSHIP****
+    signups = db.relationship('Signup', back_populates='activity')
+
 
     def __repr__(self):
         return f'<Activity {self.id}: {self.name}>'
@@ -29,9 +38,31 @@ class Activity(db.Model, SerializerMixin):
 class Camper(db.Model, SerializerMixin):
     __tablename__ = 'campers'
 
+    serialize_rules = ('-signups.camper',)
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     age = db.Column(db.Integer)
+    # created_at = db.Column(db.DateTime, server_default=db.func.now())
+    # updated_at = db.Column(db.DateTime, server_default=db.func.now(), onupdate = db.func.now())
+
+    #****RELATIONSHIP****
+    signups = db.relationship('Signup', back_populates='camper')
+    activities = association_proxy('signups', 'activity')
+
+    # Add validations to the Camper model:
+    # must have a name
+    # must have an age between 8 and 18
+    @validates('name')
+    def validate_name(self, key, name_str):
+        if not name_str:
+            raise ValueError('Camper must have a name.')
+        return name_str
+    @validates('age')
+    def validates_age(self, key, age):
+        if not 8 <= age <= 18:
+            raise ValueError('Age must be between 8 and 18')
+        return age
 
     def __repr__(self):
         return f'<Camper {self.id}: {self.name}>'
@@ -39,7 +70,29 @@ class Camper(db.Model, SerializerMixin):
 class Signup(db.Model, SerializerMixin):
     __tablename__ = 'signups'
 
+    serialize_rules = ('-activity.signups', '-camper.signups',)
+
     id = db.Column(db.Integer, primary_key=True)
+    time = db.Column(db.Integer)
+    # created_at = db.Column(db.DateTime, server_default=db.func.now())
+    # updated_at = db.Column(db.DateTime, server_default=db.func.now(), onupdate = db.func.now())    
+
+    activity_id = db.Column(db.Integer, db.ForeignKey('activities.id'))
+    camper_id = db.Column(db.Integer, db.ForeignKey('campers.id'))
+
+    
+    #****RELATIONSHIP****    
+    activity = db.relationship('Activity', back_populates='signups')
+    camper = db.relationship('Camper', back_populates='signups')
+
+    # Add validations to the Signup model:
+    # must have a time between 0 and 23 (referring to the hour of day for the activity)
+
+    @validates('time')
+    def validate_time(self, key, time):
+        if not 0 <= time <= 23:
+            raise ValueError('Time must be between 0 and 23')
+        return time
 
     def __repr__(self):
         return f'<Signup {self.id}>'
